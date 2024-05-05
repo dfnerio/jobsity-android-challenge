f704afb373a273961a5853b151ef9d12b0cf3726
dfnerio <diego9816@hotmail.com>
2024-05-05 02:20:03 -0600

    fix import

--- src/modules/favorites/selectors/getFavorites.tsx
+++ src/modules/favorites/selectors/getFavorites.tsx
@@ -1,4 +1,4 @@
-import { RootState } from '../../redux/store';
+import { RootState } from '../../../redux/store';
 
 export const getFavorites = (state: RootState) => {
   return state.favorites.value;
